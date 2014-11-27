940fac7b9c0d5ce0bc275d5dead89ad36530e243
Brian Dorey <sales@abelectronics.co.uk>
2014-11-27 21:02:55 +0000

    Updated IOPi tutorial

--- IOPi/demo-iopiread.py
+++ IOPi/demo-iopiread.py
@@ -22,28 +22,28 @@ Initialise the IOPi device using the default addresses, you will need to
 change the addresses if you have changed the jumpers on the IO Pi
 """
 i2c_helper = ABEHelpers()
-bus = i2c_helper.get_smbus()
+i2c_bus = i2c_helper.get_smbus()
 
-bus1 = IoPi(bus, 0x20)
+bus = IoPi(i2c_bus, 0x20)
 
 # We will read the inputs 1 to 8 from bus 2 so set port 0 to be inputs and
 # enable the internal pull-up resistors
-bus1.set_port_direction(0, 0xFF)
-bus1.set_port_pullups(0, 0xFF)
+bus.set_port_direction(0, 0xFF)
+bus.set_port_pullups(0, 0xFF)
 
 while True:
     # clear the console
     os.system('clear')
 
     # read the pins 1 to 8 and print the results
-    print 'Pin 1: ' + str(bus1.read_pin(1))
-    print 'Pin 2: ' + str(bus1.read_pin(2))
-    print 'Pin 3: ' + str(bus1.read_pin(3))
-    print 'Pin 4: ' + str(bus1.read_pin(4))
-    print 'Pin 5: ' + str(bus1.read_pin(5))
-    print 'Pin 6: ' + str(bus1.read_pin(6))
-    print 'Pin 7: ' + str(bus1.read_pin(7))
-    print 'Pin 8: ' + str(bus1.read_pin(8))
+    print 'Pin 1: ' + str(bus.read_pin(1))
+    print 'Pin 2: ' + str(bus.read_pin(2))
+    print 'Pin 3: ' + str(bus.read_pin(3))
+    print 'Pin 4: ' + str(bus.read_pin(4))
+    print 'Pin 5: ' + str(bus.read_pin(5))
+    print 'Pin 6: ' + str(bus.read_pin(6))
+    print 'Pin 7: ' + str(bus.read_pin(7))
+    print 'Pin 8: ' + str(bus.read_pin(8))
 
     # wait 0.5 seconds before reading the pins again
     time.sleep(0.1)
