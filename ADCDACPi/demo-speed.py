d1e5ad971a170a30e2a2ec1aa13f236a821550d4
Brian Dorey <sales@abelectronics.co.uk>
2016-01-12 13:51:59 +0000

    New speed test
    A new speed test script for the ADCDAC Pi

---
+++ ADCDACPi/demo-speed.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from ABE_ADCDACPi import ADCDACPi
+import time
+import datetime
+"""
+================================================
+ABElectronics ADCDAC Pi 2-Channel ADC, 2-Channel DAC | ADC Read Demo
+Version 1.0 Created 17/05/2014
+Version 1.1 16/11/2014 updated code and functions to PEP8 format
+run with: python demo-adcread.py
+================================================
+this demo reads the voltage from channel 1 on the ADC inputs
+"""
+
+adcdac = ADCDACPi(1)  # create an instance of the ADCDAC Pi with a DAC gain set to 1
+
+# set the reference voltage.  this should be set to the exact voltage
+# measured on the raspberry pi 3.3V rail.
+adcdac.set_adc_refvoltage(3.3)
+counter = 1
+starttime = datetime.datetime.now()
+print starttime
+while (counter < 1000000):
+    # read the voltage from channel 1 and display on the screen
+    varx =  adcdac.read_adc_voltage(1)
+    counter = counter + 1
+endtime = datetime.datetime.now()
+
+print endtime - starttime
+print endtime
