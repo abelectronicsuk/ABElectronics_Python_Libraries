e48c5db8a6dc5703f2d3280c7296bd8e436ce36a
andrew <andrew@apexweb.co.uk>
2017-06-30 10:16:28 +0100

    Fixed whitespace issue
    Fixed an issue with the ABE_helpers.py files where the whitespace was a
    mixture of spaces and tabs.

--- DeltaSigmaPi/ABE_helpers.py
+++ DeltaSigmaPi/ABE_helpers.py
@@ -9,7 +9,7 @@ import os
 """
 ================================================
 ABElectronics Python Helper Functions
-Version 1.2 Created 11/02/2017
+Version 1.3 Created 30/06/2017
 Python 2 only
 Requires python 2 smbus to be installed with: sudo apt-get install python-smbus
 This file contains functions to load puthon smbus into an instance variable.
@@ -19,23 +19,23 @@ The bus object can then be used by multiple devices without conflicts.
 
 
 class ABEHelpers:
-    
+
     def get_smbus(self):
-    	i2c_bus = 1
-    	# detect the device that is being used
-    	device = os.uname()[1]
-    	
-    	if (device == "orangepione"): # running on orange pi one
-    	    i2c_bus = 0
-    	    
-    	elif (device == "orangepiplus"): # running on orange pi one
-    	    i2c_bus = 0
-    	    
-    	elif (device == "linaro-alip"): # running on Asus Tinker Board
-    	    i2c_bus = 1
-    	    
-    	elif (device == "raspberrypi"): # running on raspberry pi
-    	
+        i2c_bus = 1
+        # detect the device that is being used
+        device = os.uname()[1]
+
+        if (device == "orangepione"): # running on orange pi one
+            i2c_bus = 0
+
+        elif (device == "orangepiplus"): # running on orange pi one
+            i2c_bus = 0
+
+        elif (device == "linaro-alip"): # running on Asus Tinker Board
+            i2c_bus = 1
+
+        elif (device == "raspberrypi"): # running on raspberry pi
+
             # detect i2C port number and assign to i2c_bus
 
             for line in open('/proc/cpuinfo').readlines():
@@ -48,9 +48,10 @@ class ABEHelpers:
                         else:
                             i2c_bus = 1
                         break
-        try:        
+        try:
             return smbus.SMBus(i2c_bus)
         except IOError:
-                print ("Could not open the i2c bus.")
-                print ("Please check that i2c is enabled and python-smbus and i2c-tools are installed.")
-                print ("Visit https://www.abelectronics.co.uk/kb/article/1/i2c--smbus-and-raspbian-linux for more information.")    
+            print("Could not open the i2c bus.")
+            print("Please check that i2c is enabled and python-smbus and i2c-tools are installed.")
+            print("For more information please visit:")
+            print("https://www.abelectronics.co.uk/kb/article/1/i2c--smbus-and-raspbian-linux")
